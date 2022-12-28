91dac8241d822c19e07521ba5abf21c38937835d
luansviana <luan.bcc1@gmail.com>
2022-12-27 22:07:34 -0300

    chore: set jest to collect coverage

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/example']
+  testPathIgnorePatterns: ['<rootDir>/example'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts*'],
+  coverageThreshold: {
+    global: {
+      lines: 95,
+      statements: 95,
+      branches: 95,
+      functions: 95
+    }
+  }
 };
